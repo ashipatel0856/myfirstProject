2a8063ff7794d7feb73c665f7f70a6a7578d809f
Ashish Kumar <ashishkumarr0856@gmail.com>
2024-12-28 11:40:49 +0530

    new

--- src/main/java/com/ashi/springBootWebTutorial/Dto/EmployeeDto.java
+++ src/main/java/com/ashi/springBootWebTutorial/Dto/EmployeeDto.java
@@ -1,4 +1,73 @@
 package com.ashi.springBootWebTutorial.Dto;
 
+import java.time.LocalDate;
+
 public class EmployeeDto {
+    private long id;
+    private String name;
+    private String email;
+    private Integer age;
+
+    private LocalDate dateOfJoining;
+
+    private Boolean isActive;
+
+    public EmployeeDto(long employeeId, String ashish, String mail, int i, LocalDate of, boolean b) {
+    }
+
+
+//
+//
+//    public EmployeeDto(String employeeID, String ashish, String mail, int i, LocalDate of, Boolean b) {
+//
+//    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(LocalDate dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
 }
